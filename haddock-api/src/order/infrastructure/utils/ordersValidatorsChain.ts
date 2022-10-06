e317eaee82f489d80fe8a5e335a6031baa97078e
Alejandro Pla <aleplacambra@gmail.com>
2022-10-06 22:30:05 +0200

    Adding one more validation

--- haddock-api/src/order/infrastructure/utils/ordersValidatorsChain.ts
+++ haddock-api/src/order/infrastructure/utils/ordersValidatorsChain.ts
@@ -4,7 +4,7 @@ import { check, ValidationChain } from 'express-validator';
  * It is validated that the json with the fields for the order are correct with the express-validator library.
  */
 export const calculateOrderValdiationChain: ValidationChain[] = [
-    check('order').isArray(),
+    check('order').isArray().isLength({ min: 1 }),
     check('order.*.number', 'number field must be a number').isNumeric(),
     check('order.*.quantity', 'quantity field must be a number').isNumeric(),
 ];
